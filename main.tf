2ae298616459ee6ec00b00daac8d8c045ab97cc6
Robert Hagen <rdh@stealthllama.org>
2023-11-21 10:35:23 -0500

    Added dhcp

--- main.tf
+++ main.tf
@@ -8,11 +8,12 @@ module "vmseries-modules_example_standalone_vmseries_with_metadata_bootstrap" {
   vmseries_image    = "vmseries-flex-byol-1110"
   ssh_keys          = var.ssh_keys
   bootstrap_options = {
+      type                = "dhcp-client"
       panorama-server     = "cloud"
       dgname              = var.folder
       dns-primary         = "8.8.8.8"
       dns-secondary       = "8.8.4.4"
-      op-command-modes    = "mgmt-interface-swap,jumbo-frame"
+      op-command-modes    = "mgmt-interface-swap"
       plugin-op-commands  = "advance-routing:enable"
       vm-series-auto-registration-pin-id    = var.cert-pin-id
       vm-series-auto-registration-pin-value = var.cert-pin-value
