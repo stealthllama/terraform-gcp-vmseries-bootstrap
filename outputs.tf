50ba71e071551b2171e3b85664b9da1d9f865452
Robert Hagen <rdh@stealthllama.org>
2023-11-21 10:37:39 -0500

    Updated outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "vmseries_address" {
-  value = module.vmseries.public_ips[0]
+  value = module.vmseries-modules_example_standalone_vmseries_with_metadata_bootstrap.vmseries_address
 }
 
 output "vmseries_ssh_command" {
-  value = "ssh admin@${module.vmseries.public_ips[0]}"
+  value = module.vmseries-modules_example_standalone_vmseries_with_metadata_bootstrap.vmseries_ssh_command
 }
