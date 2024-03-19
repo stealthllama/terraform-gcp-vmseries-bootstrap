7ded279aee8757a93660e61faf7d5c35deea523f
Robert Hagen <rdh@stealthllama.org>
2024-03-19 14:56:14 -0400

    Changed outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,6 @@
 output "fw_mgmt_public_ip" {
-  value = module.vmseries-modules_example_standalone_vmseries_with_metadata_bootstrap.vmseries_address
+  value = module.vmseries-modules_example_standalone_vmseries_with_metadata_bootstrap.vmseries_public_ips
+}
+output "fw_mgmt_private_ip" {
+  value = module.vmseries-modules_example_standalone_vmseries_with_metadata_bootstrap.vmseries_private_ips
 }
