016d544bd72157a99c71e1e7a8351cb035327f20
Robert Hagen <rdh@stealthllama.org>
2024-03-19 12:47:01 -0400

    Var changes

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 module "vmseries-modules_example_standalone_vmseries_with_metadata_bootstrap" {
   source  = "PaloAltoNetworks/vmseries-modules/google//examples/standalone_vmseries_with_metadata_bootstrap"
-  version = "1.2.6"
+  version = "1.3.3"
   name              = var.name
   project           = var.project
   region            = var.region
   allowed_sources   = ["0.0.0.0"]
-  vmseries_image    = "vmseries-flex-byol-1110"
+  vmseries_image    = "vmseries-flex-byol-1112"
   ssh_keys          = var.ssh_keys
   bootstrap_options = {
       type                = "dhcp-client"
       panorama-server     = "cloud"
       dgname              = var.folder
-      dns-primary         = "8.8.8.8"
-      dns-secondary       = "1.2.3.4"
+      #dns-primary         = "8.8.8.8"
+      #dns-secondary       = "1.2.3.4"
       #op-command-modes    = "mgmt-interface-swap"
       plugin-op-commands  = "advance-routing:enable"
       vm-series-auto-registration-pin-id    = var.cert-pin-id
